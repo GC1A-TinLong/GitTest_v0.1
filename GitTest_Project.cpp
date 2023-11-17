68a018d8144634146967c9dddccf47a20d053827
Tin Long Choung <tinlong05@gmail.com>
2023-11-17 12:05:24 +0900

    ローカルでコンフリクト変更

--- GitTest_Project.cpp
+++ GitTest_Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio git!\n";
 
-    std::cout << "GitHub Clone Changed.\n";
+    std::cout << "Conflict Changed.\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
